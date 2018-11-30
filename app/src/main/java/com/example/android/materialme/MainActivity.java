cc8a88dd70e1d8e5bb3e5054f31fc6b47ea83805
Pelane Sanon <p_sanon@aol.com>
2018-11-29 23:32:54 -0600

    check in

---
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialme;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/***
+ * Main Activity for the Material Me app, a mock Recipe news application.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<Recipe> mRecipeData;
+    private RecipeAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mRecipeData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new RecipeAdapter(this, mRecipeData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+
+        // Helper class for creating swipe to dismiss and drag and drop
+        // functionality.
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
+                .SimpleCallback(
+                    ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | 
+                    ItemTouchHelper.DOWN | ItemTouchHelper.UP,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            /**
+             * Defines the drag and drop functionality.
+             *
+             * @param recyclerView The RecyclerView that contains the list items
+             * @param viewHolder The RecipeViewHolder that is being moved
+             * @param target The RecipeViewHolder that you are switching the
+             *               original one with.
+             * @return true if the item was moved, false otherwise
+             */
+            @Override
+            public boolean onMove(RecyclerView recyclerView,
+                                  RecyclerView.ViewHolder viewHolder,
+                                  RecyclerView.ViewHolder target) {
+                // Get the from and to positions.
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+
+                // Swap the items and notify the adapter.
+                Collections.swap(mRecipeData, from, to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            /**
+             * Defines the swipe to dismiss functionality.
+             *
+             * @param viewHolder The viewholder being swiped.
+             * @param direction The direction it is swiped in.
+             */
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder,
+                                 int direction) {
+                // Remove the item from the dataset.
+                mRecipeData.remove(viewHolder.getAdapterPosition());
+                // Notify the adapter.
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+
+        // Attach the helper to the RecyclerView.
+        helper.attachToRecyclerView(mRecyclerView);
+    }
+
+    /**
+     * Initialize the Recipe data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] RecipeList = getResources()
+                .getStringArray(R.array.recipe_titles);
+        String[] RecipeInfo = getResources()
+                .getStringArray(R.array.recipe_info);
+        String[] RecipeDetail = getResources()
+                .getStringArray(R.array.recipe_details);
+        TypedArray RecipeImageResources = getResources()
+                .obtainTypedArray(R.array.recipe_images);
+
+        // Clear the existing data (to avoid duplication).
+        mRecipeData.clear();
+
+        // Create the ArrayList of Recipe objects with the titles and
+        // information about each Recipe
+        for (int i = 0; i < RecipeList.length; i++) {
+            mRecipeData.add(new Recipe(RecipeList[i], RecipeInfo[i], RecipeDetail[i],
+                    RecipeImageResources.getResourceId(i, 0)));
+        }
+
+        // Recycle the typed array.
+        RecipeImageResources.recycle();
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * onClick method for th FAB that resets the data.
+     *
+     * @param view The button view that was clicked.
+     */
+    public void resetRecipe(View view) {
+        initializeData();
+    }
+}
---
+++ app/src/main/java/com/example/android/materialme/Recipe.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialme;
+
+/**
+ * Data model for each row of the RecyclerView
+ */
+class Recipe {
+
+    // Member variables representing the title and information about the Recipe.
+    private String title;
+    private String info;
+    private String details;
+    private final int imageResource;
+
+    /**
+     * Constructor for the Recipe data model.
+     *
+     * @param title The name if the Recipe.
+     * @param info Information about the Recipe.
+     */
+    public Recipe(String title, String info, String details, int imageResource) {
+        this.title = title;
+        this.info = info;
+        this.details = details;
+        this.imageResource = imageResource;
+    }
+
+    /**
+     * Gets the title of the Recipe.
+     *
+     * @return The title of the Recipe.
+     */
+    String getTitle() {
+        return title;
+    }
+
+    /**
+     * Gets the info about the Recipe.
+     *
+     * @return The info about the Recipe.
+     */
+    String getInfo() {
+        return info;
+    }
+
+    /**
+     * Gets the details about the Recipe.
+     *
+     * @return The details about the Recipe.
+     */
+    String getDetails() {
+        return details;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+}
